4098002d25b7eeb3549fab6029a57483024bb213
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2020-09-22 00:20:37 +0700

    config eslint + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,45 @@
 module.exports = {
-  "extends": ["react-app"]
+  extends: [
+    "react-app",
+    "plugin:json/recommended",
+    "plugin:prettier/recommended",
+    "prettier/react",
+    "prettier/standard",
+    "prettier/babel",
+    // "prettier/@typescript-eslint"
+  ],
+  parser: "babel-eslint",
+  // parser: "@typescript-eslint/parser",
+  // overrides: [
+  //   {
+  //     files: ["*.mjs", "*.json"],
+  //     parser: "babel-eslint",
+  //   },
+  // ],
+  rules: {},
+  settings: {
+    "react": {
+      "version": "detect"
+    },
+  },
+  env: {
+    es6: true,
+    es2017: true,
+    es2020: true,
+    browser: true,
+    node: true,
+    worker: true,
+    serviceworker: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    },
+    // project: './tsconfig.json',
+    project: './jsconfig.json',
+  },
+  globals: {}
 }
---
+++ src/request/constant.js
@@ -0,0 +1,13 @@
+export default {
+  HTTP_CODE: {
+    OK: 200,
+    CREATED: 201,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    METHOD_NOT_ALLOWED: 405,
+    INTERNAL_SERVER_ERROR: 500,
+    BAD_GATEWAY: 502,
+  },
+};
