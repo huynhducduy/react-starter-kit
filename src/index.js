87977cc546405b8daeda41b9e0c1ee5ffee08348
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2020-09-21 23:36:40 +0700

    HMR enabled

--- src/index.js
+++ src/index.js
@@ -4,12 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function render(Component) {
+  return ReactDOM.render(
+    <React.StrictMode>
+      {/* Providers */}
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+render(App);
+
+// Enable hot-module-replacement https://medium.com/@brianhan/hot-reloading-cra-without-eject-b54af352c642
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    render(NextApp);
+  });
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
