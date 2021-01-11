d64dcfd12fb746f3a36023bd025f5e4e66499a01
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2021-01-12 01:19:34 +0700

    update config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
-export default {
+const config = {
   name: process.env.REACT_APP_NAME, // Using as prefix for localStorage
 };
+
+export default config;
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,11 @@ import Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
 
 configure({ adapter: new Adapter() });
+
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+};
+global.localStorage = localStorageMock;
