914e231e39b4f97e6d6fc6246f26d4405c8fcc4e
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2021-03-08 17:20:55 +0700

    update localStorage util

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -4,17 +4,24 @@ function generateKey(key: string) {
   return `${String(config.name)}_${key}` // prefix_key
 }
 
+/**
+ * storage event fired only on the source (tab - window) that did not originate the change
+ */
+
 export function clear(): void {
   if (typeof localStorage !== 'undefined') {
+    if (localStorage.length > 0) {
+      window.dispatchEvent(
+        new StorageEvent('storage', {
+          key: null,
+          newValue: null,
+          oldValue: null,
+          url: window.location.href,
+          storageArea: localStorage,
+        })
+      )
+    }
     localStorage.clear()
-    const event = new StorageEvent('localStorage', {
-      key: null,
-      newValue: null,
-      oldValue: null,
-      url: window.location.href,
-      storageArea: localStorage,
-    })
-    window.dispatchEvent(event)
   }
 }
 
@@ -28,30 +35,36 @@ export function get(key: string): string | null {
 export function set(key: string, value: string): void {
   if (typeof localStorage !== 'undefined') {
     const k = generateKey(key)
-    const event = new StorageEvent('localStorage', {
-      key: k,
-      newValue: value,
-      oldValue: localStorage.getItem(k),
-      url: window.location.href,
-      storageArea: localStorage,
-    })
+    if (localStorage.getItem(k) !== value) {
+      window.dispatchEvent(
+        new StorageEvent('storage', {
+          key: k,
+          newValue: value,
+          oldValue: localStorage.getItem(k),
+          url: window.location.href,
+          storageArea: localStorage,
+        })
+      )
+    }
     localStorage.setItem(k, value)
-    window.dispatchEvent(event)
   }
 }
 
 export function remove(key: string): void {
   if (typeof localStorage !== 'undefined') {
     const k = generateKey(key)
-    const event = new StorageEvent('localStorage', {
-      key: k,
-      newValue: null,
-      oldValue: localStorage.getItem(k),
-      url: window.location.href,
-      storageArea: localStorage,
-    })
+    if (localStorage.getItem(k) !== null) {
+      window.dispatchEvent(
+        new StorageEvent('storage', {
+          key: k,
+          newValue: null,
+          oldValue: localStorage.getItem(k),
+          url: window.location.href,
+          storageArea: localStorage,
+        })
+      )
+    }
     localStorage.removeItem(k)
-    window.dispatchEvent(event)
   }
 }
 
