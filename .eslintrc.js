7ac1c3aa1cdbcee0a52d1f491e5ef0eedfb63cee
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2021-03-09 18:38:18 +0700

    add cypress e2e

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 var restrictedGlobals = require('confusing-browser-globals')
 
 var extendsConfig = [
+  'plugin:cypress/recommended',
   'plugin:json/recommended',
   'plugin:prettier/recommended', // Always the last: https://github.com/prettier/eslint-plugin-prettier#recommended-configuration
   'prettier/prettier', // Fix some errors: https://github.com/prettier/eslint-plugin-prettier#arrow-body-style-and-prefer-arrow-callback-issue
