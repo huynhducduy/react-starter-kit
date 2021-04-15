8fff86304730b0df5aff4f78034f8feec8b0175d
Huynh Duc Duy <duy.huynh@linecorp.com>
2021-04-15 17:36:24 +0700

    enable tailwindcss is now an option

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,18 @@
+require('dotenv').config()
+
+const ENABLE_TAILWINDCSS = process.env.ENABLE_TAILWINDCSS === 'true'
+
 module.exports = (api) => {
   return {
     plugins: [
       require('postcss-flexbugs-fixes'),
-      require('tailwindcss'),
+      ENABLE_TAILWINDCSS && require('tailwindcss'),
       require('postcss-preset-env')({
         stage: 1,
       }),
       require('autoprefixer'),
-      // require('postcss-normalize'), // Tailwind already come with modern-normalize
-      require('cssnano'),
-    ],
+      !ENABLE_TAILWINDCSS && require('postcss-normalize'), // Tailwind already come with modern-normalize
+      // require('cssnano'), // Already have optimize-css-assets-webpack-plugin in webpack
+    ].filter(Boolean),
   }
 }
