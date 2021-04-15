81c5d6d6379cf1b2214d0123017ad518cac1c137
Huynh Duc Duy <duy.huynh@linecorp.com>
2021-04-15 15:52:15 +0700

    resolve react warning

--- src/components/LanguageChanger/index.tsx
+++ src/components/LanguageChanger/index.tsx
@@ -3,9 +3,12 @@ import i18n from 'i18n'
 
 const LanguageChanger = () => {
   return (
-    <select onChange={(e) => i18n.changeLanguage(e.target.value)}>
+    <select
+      onChange={(e) => i18n.changeLanguage(e.target.value)}
+      defaultValue={i18n.language}
+    >
       {availableLocales.map((locale) => (
-        <option value={locale} selected={locale === i18n.language}>
+        <option value={locale} key={locale}>
           {locale}
         </option>
       ))}
